238d2ed8d341e0d775e8de57707d7d8c8f92ad84
Prateek Saxena <prtksxna@gmail.com>
2015-03-21 14:55:11 +0530

    Fix indentation

--- src/app.js
+++ src/app.js
@@ -1,27 +1,21 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var ajax = require('ajax');
 
 
 // Init
 var gettingLocationCard = new UI.Card({
-  title: 'Nearby',
-  subtitle: 'Getting location…'
+	title: 'Nearby',
+	subtitle: 'Getting location…'
 });
 
 var loadingArticlesCard = new UI.Card({
-  title: 'Nearby',
-  subtitle: 'Loading Wikipedia articles…'
+	title: 'Nearby',
+	subtitle: 'Loading Wikipedia articles…'
 });
 
 var errorCard = new UI.Card({
-  title: 'Error',
-  subtitle: 'There was some problem'
+	title: 'Error',
+	subtitle: 'There was some problem'
 })
 
 gettingLocationCard.show();
@@ -29,48 +23,48 @@ gettingLocationCard.show();
 
 // Location
 var locationOptions = {
-  enableHighAccuracy: true, 
-  maximumAge: 10000, 
-  timeout: 10000
+	enableHighAccuracy: true,
+	maximumAge: 10000,
+	timeout: 10000
 };
 
 function locationSuccess(pos) {
-  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  loadingArticlesCard.show();
-  loadArticles( pos.coords.latitude, pos.coords.longitude );
+	console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+	loadingArticlesCard.show();
+	loadArticles( pos.coords.latitude, pos.coords.longitude );
 }
 
 function locationError(err) {
-  console.log('location error (' + err.code + '): ' + err.message);
-  errorCard.show()
+	console.log('location error (' + err.code + '): ' + err.message);
+	errorCard.show()
 }
 
 Pebble.addEventListener('ready',
-  function(e) {
-    // Request current position
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-  }
+	function(e) {
+		// Request current position
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+	}
 );
 
 // Wikipedia
 function loadArticles ( lat, lon ) {
-  var url = 'http://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=' + lat + '|' + lon + '&format=json';
-  ajax({url: url, type: 'json'},
-  function(json) {
-    showPlaces( json.query.geosearch )
-  },
-  function(error) {
-    console.log('Ajax failed: ' + error);
-  }
+	var url = 'http://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=' + lat + '|' + lon + '&format=json';
+	ajax({url: url, type: 'json'},
+	function(json) {
+		showPlaces( json.query.geosearch )
+	},
+	function(error) {
+		console.log('Ajax failed: ' + error);
+	}
 );
 }
 
-function showPlaces ( places ){  
-  var sections = [];
-  for( var i = 0; i < places.length; i ++)   {
-    var place = places[ i ];
-    sections.push( {title: place.title })
-  }
-  var placeMenu = new UI.Menu({ sections: sections });
-  placeMenu.show();
+function showPlaces ( places ){
+	var sections = [];
+	for( var i = 0; i < places.length; i ++)	 {
+		var place = places[ i ];
+		sections.push( {title: place.title })
+	}
+	var placeMenu = new UI.Menu({ sections: sections });
+	placeMenu.show();
 }
