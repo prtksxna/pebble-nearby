1d72cc5dd082619855fcd924826968806e318665
Prateek Saxena <prtksxna@gmail.com>
2015-07-04 16:50:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nearby
+
+A [Pebble](https://getpebble.com/#/xfl3ZU:y7yM) app that fetches Wikipedia articles near you.
+
+![pebble](http://prtksxna.com/wp-content/uploads/2015/03/pebble.gif)
+![pebble-article](http://prtksxna.com/wp-content/uploads/2015/03/pebble-article.gif)
+![pebble-refresh](http://prtksxna.com/wp-content/uploads/2015/03/pebble-refresh.gif)
+
+Not on the Pebble app store yet. Waiting until all [issues](https://github.com/prtksxna/pebble-nearby/issues) are resolved. You can install a [pre-release](https://github.com/prtksxna/pebble-nearby/releases) until then.~
---
+++ src/js/app.js
@@ -0,0 +1,130 @@
+var UI = require('ui');
+var ajax = require( 'ajax' );
+
+var Cards = {
+	gettingLocation: new UI.Card( {
+		title: 'Nearby',
+		subtitle: 'Getting location…'
+	} ),
+
+	loadingArticles: new UI.Card( {
+		title: 'Nearby',
+		subtitle: 'Loading Wikipedia articles…'
+	} ),
+
+	error: new UI.Card( {
+		title: 'Error',
+		subtitle: 'There was some problem'
+	} )
+};
+
+var Location = {
+	options: {
+		enableHighAccuracy: true,
+		maximumAge: 10000,
+		timeout: 10000
+	},
+
+	successCallback: function ( pos ) {
+		console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+		Cards.loadingArticles.show();
+		Cards.gettingLocation.hide();
+		WP.loadArticles( pos.coords.latitude, pos.coords.longitude );
+	},
+
+	errorCallback: function ( err ) {
+		console.log('location error (' + err.code + '): ' + err.message);
+		Cards.error.show();
+	},
+
+	getCurrentLocation: function () {
+		Cards.gettingLocation.show();
+		console.log( 'getting location ');
+		navigator.geolocation.getCurrentPosition(
+			Location.successCallback,
+			Location.errorCallback,
+			Location.options
+		);
+	}
+};
+
+var WP = {
+	loadArticles: function ( lat, lon ) {
+		var url = 'http://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=' + lat + '|' + lon + '&format=json';
+		ajax( { url: url, type: 'json' },
+							function(json) {
+								WP.showPlaces( json.query.geosearch, { latitude: lat, longitude: lon } );
+							},
+							function(error) {
+								Cards.error.show();
+							}
+						);
+	},
+
+	showArticle: function ( title, distance, pageid ) {
+		// open a card
+		var loadingCard = new UI.Card( {
+			title: title,
+			subtitle: 'Loading…'
+		} ).show();
+
+		// make ajax call
+		var url = 'http://en.wikipedia.org/w/api.php?format=json&action=query&pageids=' + pageid + '&prop=extracts&explaintext=true&exintro=true&exsentences=2';
+		ajax( { url: url, type: 'json' },
+							function ( json ) {
+								new UI.Card( {
+									title: title,
+									subtitle: distance,
+									body: json.query.pages[ pageid ].extract,
+									scrollable: true,
+									style: 'small'
+								} ).show();
+								loadingCard.hide();
+							},
+							function ( error ) {
+								Cards.error.show();
+							}
+		);
+	},
+
+	showPlaces: function ( places, currentLocation ) {
+		var items = [], placeMenu;
+		for( var i = 0; i < places.length; i ++) {
+			var place = places[ i ];
+			items.push( {
+				title: place.title,
+				subtitle: Math.ceil( place.dist / 1000 ) + ' km',
+				pageid: place.pageid
+			} );
+		}
+		placeMenu = new UI.Menu( {
+			sections: [
+				{
+					title: 'Get new places',
+					items: [ {
+						title: 'Refresh',
+						subtitle: 'Get you current location…'
+					} ]
+				},
+				{
+					title: 'Places',
+					items: items
+				}
+			]
+		} );
+
+		placeMenu.on( 'select', function ( e ) {
+			if ( e.itemIndex === 0 && e.sectionIndex === 0 ){
+				Location.getCurrentLocation();
+			} else {
+				WP.showArticle( e.item.title, e.item.subtitle, e.item.pageid );
+			}
+		} );
+
+		placeMenu.show();
+		Cards.loadingArticles.hide();
+	}
+};
+
+// Init
+Location.getCurrentLocation();
